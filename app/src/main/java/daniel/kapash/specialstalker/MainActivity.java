d82e6e75c3f98460ea8bbbe796caab7cce199db3
Daniel <danielkpsh@gmail.com>
2019-04-25 20:24:56 +0300

    get permissions

--- app/src/main/java/daniel/kapash/specialstalker/MainActivity.java
+++ app/src/main/java/daniel/kapash/specialstalker/MainActivity.java
@@ -1,13 +1,80 @@
 package daniel.kapash.specialstalker;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST = 17;
+
+    private AlertDialog permissionsExplanationDialog;
+    private AlertDialog exitDialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("The app can't work without all the requested permissions");
+        alertDialogBuilder.setPositiveButton("Got it", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.PROCESS_OUTGOING_CALLS,
+                                Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST);
+            }
+        });
+        permissionsExplanationDialog = alertDialogBuilder.create();
+
+        alertDialogBuilder.setMessage("The app can't work without all the requested permissions, please allow permissions in settings.");
+        alertDialogBuilder.setPositiveButton("Got it", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        exitDialog = alertDialogBuilder.create();
+
+        getPermissions();
+
     }
+
+    private void getPermissions() {
+        Boolean deniedReadPhoneStatePermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED;
+        Boolean deniedProcessOutgoingCallsPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.PROCESS_OUTGOING_CALLS)
+                != PackageManager.PERMISSION_GRANTED;
+        Boolean deniedSendSmsPermission = ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED;
+
+        if (deniedProcessOutgoingCallsPermission || deniedReadPhoneStatePermission || deniedSendSmsPermission) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_PHONE_STATE, Manifest.permission.PROCESS_OUTGOING_CALLS,
+                            Manifest.permission.SEND_SMS},
+                    MY_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED ||
+                grantResults[1] != PackageManager.PERMISSION_GRANTED ||
+                grantResults[2] != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.PROCESS_OUTGOING_CALLS) ||
+                    ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) {
+                permissionsExplanationDialog.show();
+            } else
+                exitDialog.show();
+        }
+    }
+    
 }
