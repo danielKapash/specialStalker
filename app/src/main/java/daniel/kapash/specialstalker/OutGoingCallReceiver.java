74a261e7fa96da1b5721f4284830fb1f17176467
Daniel <danielkpsh@gmail.com>
2019-04-28 14:27:22 +0300

    Started ex7

--- app/src/main/java/daniel/kapash/specialstalker/OutGoingCallReceiver.java
+++ app/src/main/java/daniel/kapash/specialstalker/OutGoingCallReceiver.java
@@ -1,17 +1,23 @@
 package daniel.kapash.specialstalker;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.telephony.SmsManager;
 import android.util.Log;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-
 public class OutGoingCallReceiver extends BroadcastReceiver {
+
+    private static final String STALKER_NOTIFICATION_CHANNEL_ID = "notification_channel_id";
+    public static final int NOTIFICATION_ID = 12345;
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -22,7 +28,41 @@ public class OutGoingCallReceiver extends BroadcastReceiver {
         String outgoingCallPhoneNumber = intent.getStringExtra(Intent.EXTRA_PHONE_NUMBER);
         String smsMessage = smsPrefix + outgoingCallPhoneNumber;
 
+        createNotificationChannel(context);
+
+        Notification notification = getSendingSmsNotification(context);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+        notificationManager.notify(NOTIFICATION_ID, notification);
+
+
         SmsManager.getDefault().sendTextMessage(phoneNumberToSendSmsTo, null, smsMessage, null, null);
         Log.d("OutGoingCallReceiver", "onReceive");
     }
+
+    private Notification getSendingSmsNotification(Context context) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, STALKER_NOTIFICATION_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle("Stalker Activated")
+                .setContentText("Sending message...")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        return builder.build();
+    }
+
+    private void createNotificationChannel(Context context) {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(STALKER_NOTIFICATION_CHANNEL_ID, "Notification", importance);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+
+            NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
